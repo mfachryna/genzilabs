fac6a28b735b5e6401836e5021302df4c66a7cea
mfachryna <fachry.arifna@gmail.com>
2026-01-04 21:19:37 +0700

    fix: routes missing Quotes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,11 +6,7 @@
     import About from '$lib/components/landing/About.svelte';
     import WhatWeBuild from '$lib/components/landing/WhatWeBuild.svelte';
     import HowWeWork from '$lib/components/landing/HowWeWork.svelte';
-    import Quote from '$lib/components/landing/Quote.svelte';
-    import Pillars from '$lib/components/landing/Pillars.svelte';
     import IdeaCTA from '$lib/components/landing/IdeaCTA.svelte';
-    import Team from '$lib/components/landing/Team.svelte';
-    import JoinCircle from '$lib/components/landing/JoinCircle.svelte';
     import Footer from '$lib/components/landing/Footer.svelte';
 </script>
 
